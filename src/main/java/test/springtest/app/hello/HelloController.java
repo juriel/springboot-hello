c0b8de1cabe6126127d61a863582e240c867fb0c
jaime <jaime@DESKTOP-32DIB47>
2022-04-11 13:06:53 -0500

    Hello World

--- src/main/java/test/springtest/app/hello/HelloController.java
+++ src/main/java/test/springtest/app/hello/HelloController.java
@@ -3,6 +3,7 @@ package test.springtest.app.hello;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 /**
  * @author jaime
  */
